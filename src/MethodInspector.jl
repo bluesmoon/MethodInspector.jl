748a0dd3e841ec1cdc48fd994ce63aded81a7944
Philip Tellis <philip.tellis@gmail.com>
2024-05-28 15:17:35 -0400

    Add implementation and tests

--- src/MethodInspector.jl
+++ src/MethodInspector.jl
@@ -1,2 +1,205 @@
+"""
+Inspect the names and types of positional and keyword arguments to a method
+"""
 module MethodInspector
+
+export
+    kwarg_types,
+    kwarg_names,
+    arg_types,
+    arg_names
+
+"""
+* `kwarg_types(::Method)` → `Type[]`
+
+Given a Method, return a list of types of the keyword warguments supported by that method.
+
+### Arguments
+
+`m::Method`
+: The method to check for `kwargs`
+
+### Returns
+
+A `Type` array of the kwarg types supported by method `m`. Arg names can be fetched using [`kwarg_names`](@ref). Both functions will
+return values in the same order. Methods that do not support `kwargs` will return an empty array.
+
+### Examples
+
+```julia
+kwarg_types(foo)
+```
+"""
+function kwarg_types(m::Method)
+    real_fn_ref = Base.uncompressed_ast(m).code
+
+    if length(real_fn_ref) <= 1 || !isa(real_fn_ref[end-1].args[1] , GlobalRef)
+        # This method does not have kwargs
+        return Type[]
+    end
+
+    if VERSION < v"1.10" || real_fn_ref[end-1].args[1].name != :_apply_iterate
+        real_fn_ref = real_fn_ref[end-1].args
+    end
+
+    arg_idx = findfirst(a -> GlobalRef == typeof(a) && a.mod == m.module, real_fn_ref)
+
+    if isnothing(arg_idx)
+        @warn "Could not find $(m.name) in $(m.module)"
+        return Type[]
+    end
+
+    real_fn_ref = real_fn_ref[arg_idx]
+
+    real_fn = eval(real_fn_ref)
+    real_mt = only(methods(real_fn))
+
+    sig = Base.unwrap_unionall(real_mt.sig)
+
+    last_param = findfirst(p -> p isa DataType && p.name == m.sig.parameters[1].name, sig.parameters)
+
+    params = collect(sig.parameters[2:last_param-1])
+
+    # Get rid of parameters from parameterized types
+    for i in 1:length(params)
+        while params[i] isa TypeVar
+            params[i] = params[i].ub
+        end
+    end
+
+    return Vector{Type}(params)
+end
+kwarg_types(m::Union{Vector{Method},Base.MethodList}) = kwarg_types(first(m.ms))
+
+
+"""
+* `kwarg_names(::Method)` → `Symbol[]`
+* `kwarg_names(::Base.MethodList)` → `Symbol[]`
+
+Given a method, return the names of all Keyword Arguments of that method.
+Alternately, given a MethodList, return the names all all keyword arguments accepted by the method that accepts the most keyword arguments.
+
+### Arguments
+
+`m::Method`
+: The method to check for `kwargs`
+
+`mts::Base.MethodList`
+: A MethodList (returned by `methods(...)`) to search for keyword arguments.
+
+### Returns
+
+A `Symbol` array of the kwargs supported by method `m`. Arg types can be fetched using [`kwarg_types`](@ref) which returns types in the same order.
+Default values are not returned. Methods that do not support `kwargs` will return an empty array.
+
+### Examples
+
+```julia
+kwarg_names(methods(foo).ms[1])
+
+kwarg_names(methods(foo, (Int,), MyModule))
+```
+"""
+function kwarg_names(m::Method)
+    kwargs = Base.kwarg_decl(m)
+
+    if isa(kwargs, Vector) && length(kwargs) > 0
+        kwargs = filter(!occursin("#") ∘ string, kwargs)
+
+        # Keywords *may* not be sorted correctly. We move the vararg one to the end.
+        index = something(findfirst(arg -> endswith(string(arg), "..."), kwargs), 0)
+        if index > 0
+            kwargs[index], kwargs[end] = kwargs[end], kwargs[index]
+        end
+    end
+
+    return kwargs
+end
+function kwarg_names(mts::Union{Vector{Method},Base.MethodList})
+    potential_args = Symbol[]
+
+    for mt in mts
+        kwargs = kwarg_names(mt)
+
+        if length(kwargs) > length(potential_args)
+            potential_args = kwargs
+        end
+    end
+
+    return potential_args
+end
+
+
+"""
+* `arg_names(::Method)` → `Symbol[]`
+* `arg_names(::Base.MethodList)` → `Symbol[]`
+
+Given a method, return a list of args required by that method.
+If given a MethodList instead, use the first Method in that MethodList.
+
+### Arguments
+
+`m::Base.MethodList|Method`
+: The Method or MethodList to check for `args`
+
+#### Returns
+
+A `Symbol` array of the args required by the first method passed in. Arg types and default values are not returned.  Functions with no required `args` will return an empty array.
+
+### Examples
+
+```julia
+arg_names(foo)
+```
+"""
+arg_names(mts::Union{Vector{Method},Base.MethodList}) = arg_names(first(mts))
+arg_names(mt::Method)   = Base.method_argnames(mt)[2:end]
+
+
+
+"""
+* `arg_types(::Method)` → `Type[]`
+
+Given a method, return the datatype of all its positional arguments.
+
+### Arguments
+
+`m::Base.MethodList|Method`
+: The Method or MethodList to check for `args`
+
+### Returns
+
+A `Type` array of the arg types supported by method `m`. Arg names can be fetched using [`arg_names`](@ref). Both functions will
+return values in the same order. Methods that do not support `args` will return an empty array.
+
+### Examples
+
+```julia
+arg_types(foo)
+```
+"""
+function arg_types(mt::Method)
+    type_tuple = Base.arg_decl_parts(mt)
+
+    type_strings = map(x -> x[2], filter(x -> !isempty(x[1]), type_tuple[2]))
+    type_params  = type_tuple[1]
+
+    return convert(Vector{Type}, map(type_strings) do arg_type
+        if isempty(arg_type)
+            return Any
+        else
+            # Now replace any parameters that didn't have a parent type (i.e., were just Any)
+            for type_param in type_params
+                param = type_param.name
+                ub    = type_param.ub
+
+                arg_type = replace(arg_type, Regex("(?:<:)?\\b$(param)\\b") => "<:$(ub)")
+            end
+
+            return eval(Meta.parse(arg_type))
+        end
+    end)
+end
+arg_types(mts::Union{Vector{Method},Base.MethodList}) = arg_types(first(mts))
+
 end
---
+++ test/runtests.jl
@@ -0,0 +1,133 @@
+using MethodInspector, Test, InteractiveUtils
+
+test_nokw() = "0_0k"
+test_nokw(a::Vector{<:Real}) = "1cp_0k"
+test_nokw(a::Int, b::Vector{<:T}) where T = "1vp_0k"
+test_nokw(a::Char, b::Vector{T}) where T <: Real = "1vp_0k"
+test_nokw(a::Float64, b::Tuple{<:T,<:Real}) where T <: AbstractString = "1vp_0k"
+test_nokw(a::Bool, b::Tuple{<:Real,U}) where U = "1vp_0k"
+test_nokw(a::Unsigned, b::Tuple{<:Real,U,<:V}) where {U, V<:AbstractFloat} = "1vp_0k"
+
+test_kw()  = "0_0k"
+test_kw(a) = "1_0k"
+test_kw(a::Int) = "1t_0k"
+test_kw(a::Bool; b=10) = "1t_1k"
+test_kw(a::Char; b::Int=20) = "1t_1tk"
+test_kw(a::Float64, b::Char='a'; c::Int=30) = "1t_1d_1tk"
+test_kw(a::AbstractString, b...; c::Int=30) = "1t_1v_1tk"
+test_kw(a::Complex; c::Int=30, d...) = "1t_1tk_1kv"
+test_kw(a::Symbol; c::Int=30, d::Float64=1., e::AbstractString="", f::Symbol=:a, g::Bool=false, h::Bool=true) = "1t_6k"
+test_kw(a::Rational; c::Int=30, d::Float64=1., e::AbstractString="", f::Symbol=:a, g::Bool=false, h::Bool=true, i::Val{T}=Val(:foo), j::J) where {T, J<:AbstractString} = "1t_6k_2p"
+
+test_opt_kw(a::Complex, b::Complex, c::Symbol = :c; d::Int=30, e::Float64=1., f::AbstractString="", g::Symbol=:a, h::Bool=false) = "2t_1o_5k"
+
+@testset "Initializations" begin
+    @test test_nokw() == "0_0k"
+
+    @test test_kw() == "0_0k"
+
+    @test test_kw([]) == "1_0k"
+
+    @test test_kw(10) == "1t_0k"
+
+    @test test_kw(true) == "1t_1k"
+    @test test_kw(true, b=5) == "1t_1k"
+
+    @test test_kw('a') == "1t_1tk"
+    @test test_kw('a', b=5) == "1t_1tk"
+
+    @test test_kw(1.) == "1t_1d_1tk"
+    @test test_kw(1., c=5) == "1t_1d_1tk"
+    @test test_kw(1., 'b') == "1t_1d_1tk"
+    @test test_kw(1., 'b', c=5) == "1t_1d_1tk"
+
+    @test test_kw("A") == "1t_1v_1tk"
+    @test test_kw("A", 3) == "1t_1v_1tk"
+    @test test_kw("A", c=5) == "1t_1v_1tk"
+    @test test_kw("A", 3, c=5) == "1t_1v_1tk"
+
+    @test test_kw(10+5im) == "1t_1tk_1kv"
+    @test test_kw(10+5im, c=5) == "1t_1tk_1kv"
+    @test test_kw(10+5im, d=5) == "1t_1tk_1kv"
+    @test test_kw(10+5im, x=5) == "1t_1tk_1kv"
+    @test test_kw(10+5im, c=5, x=5) == "1t_1tk_1kv"
+    @test test_kw(10+5im, c=5, x=5, y=6) == "1t_1tk_1kv"
+
+    @test test_kw(:a) == "1t_6k"
+    @test test_kw(:a, c=10) == "1t_6k"
+end
+
+@testset "kwarg_names" begin
+    @test kwarg_names(methods(test_nokw)) == Symbol[]
+
+    @test kwarg_names(methods(test_kw, (Symbol,)).ms[1]) == Symbol[:c, :d, :e, :f, :g, :h]
+    @test kwarg_names(methods(test_kw)) == Symbol[:c, :d, :e, :f, :g, :h, :i, :j]
+
+    @test kwarg_names(methods(test_kw, (Symbol,))) == Symbol[:c, :d, :e, :f, :g, :h]
+    @test kwarg_names(methods(test_kw, (Rational,))) == Symbol[:c, :d, :e, :f, :g, :h, :i, :j]
+
+    @test kwarg_names(methods(test_kw, (Bool,))) == Symbol[:b]
+    @test kwarg_names(methods(test_kw, (Char,))) == Symbol[:b]
+    @test kwarg_names(methods(test_kw, (Int64,))) == Symbol[]
+    if VERSION < v"1.10"
+        @test kwarg_names(methods(test_kw, (Float64,))) == Symbol[]
+    else
+        @test kwarg_names(methods(test_kw, (Float64,))) == Symbol[:...]
+    end
+    @test kwarg_names(methods(test_kw, (Float64, Char))) == Symbol[:c]
+    @test kwarg_names(methods(test_kw, (Complex,))) == Symbol[:c, Symbol("d...")]
+    @test kwarg_names(methods(test_kw, (Type{Symbol("")},))) == Symbol[]
+
+    @test kwarg_names(methods(test_opt_kw)) == Symbol[:d, :e, :f, :g, :h]
+end
+
+@testset "kwarg_types" begin
+    @test Type[]    == kwarg_types(methods(test_kw, (Int,)))
+    @test Type[Any] == kwarg_types(methods(test_kw, (Bool,)))
+    @test Type[Int] == kwarg_types(methods(test_kw, (Char,)))
+    @test Type[Int] == kwarg_types(methods(test_kw, (Float64, Char,)))
+    @test Type[Int] == kwarg_types(methods(test_kw, (AbstractString,Vector,)))
+    typs = kwarg_types(methods(test_kw, (Complex,)))
+    if VERSION < v"1.10.3"
+        @test Type[Int, Any] == typs
+    else
+        @test all(Type[Int, Any] .>: typs)
+    end
+    @test Type[Int, Float64, AbstractString, Symbol, Bool, Bool] == kwarg_types(methods(test_kw, (Symbol,)))
+    @test all(Type[Int, Float64, AbstractString, Symbol, Bool, Bool, Val, AbstractString] .>: kwarg_types(methods(test_kw, (Rational,))))
+end
+
+@testset "arg_types" begin
+    # Returns the first method
+    @test Type[] == arg_types(methods(test_kw, tuple()))
+    @test Type[Char] == arg_types(methods(test_kw, (Char,)).ms[1])
+    @test isempty(setdiff(arg_types(methods(test_kw, (Number,))), [Int, Float64, Bool, Rational]))   # The first matching method will differ across Julia versions
+    @test isempty(setdiff(arg_types(methods(test_kw, (Real,))), [Int, Float64, Bool, Rational]))   # The first matching method will differ across Julia versions
+    @test Type[AbstractString, Any] == arg_types(methods(test_kw, (AbstractString,)))
+    @test Type[Rational] == arg_types(methods(test_kw, (Rational,)))
+
+    @test Type[] == arg_types(methods(test_nokw))
+    @test Type[Vector{<:Real},] == arg_types(methods(test_nokw, (Vector,)))
+    @test Type[Int, Vector{<:Any},] == arg_types(methodswith(Int, test_nokw))
+    @test Type[Char, Vector{<:Real},] == arg_types(methodswith(Char, test_nokw))
+    @test Type[Float64, Tuple{<:AbstractString,<:Real},] == arg_types(methodswith(Float64, test_nokw))
+    @test Type[Bool, Tuple{<:Real,<:Any},] == arg_types(methodswith(Bool, test_nokw))
+    @test Type[Unsigned, Tuple{<:Real,Any,<:AbstractFloat},] == arg_types(methodswith(Unsigned, test_nokw))
+end
+
+@testset "arg_names" begin
+    @test Symbol[] == arg_names(methods(test_kw, tuple()))
+    @test Symbol[:a] == arg_names(methods(test_kw, (Char,)).ms[1])
+    @test [:a] == arg_names(methods(test_kw, (Number,)))
+    @test [:a] == arg_names(methods(test_kw, (Real,)))
+    @test [:a, :b] == arg_names(methods(test_kw, (AbstractString,)))
+    @test [:a] == arg_names(methods(test_kw, (Rational,)))
+
+    @test Symbol[] == arg_names(methods(test_nokw))
+    @test [:a] == arg_names(methods(test_nokw, (Vector,)))
+    @test [:a, :b,] == arg_names(methodswith(Int, test_nokw))
+    @test [:a, :b,] == arg_names(methodswith(Char, test_nokw))
+    @test [:a, :b,] == arg_names(methodswith(Float64, test_nokw))
+    @test [:a, :b,] == arg_names(methodswith(Bool, test_nokw))
+    @test [:a, :b,] == arg_names(methodswith(Unsigned, test_nokw))
+end
